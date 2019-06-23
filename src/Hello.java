ce94e1b8674e669f39a3335d6117d407bc2919aa
ss100yan <32798618+ss100yan@users.noreply.github.com>
2019-06-23 11:28:09 -0400

    firs commit

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+
+    public static void main(String[]args){
+
+        System.out.println("Hello, World!");
+
+        int myFirstNumber =(99 +5)+2*(59-1);
+        System.out.println(myFirstNumber);
+        System.out.println("myFirstNumber");
+    }
+}
